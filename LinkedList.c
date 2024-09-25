ad8a9a79d4ff3b191792cd95ec1440159e8929f1
jeswin <jeswinpeter92@gmail.com>
2024-09-25 22:28:04 +0530

    Tying out Linked list

--- LinkedList.c
+++ LinkedList.c
@@ -1,7 +1,15 @@
 //This is a program implementing concept of Singly Linked List
+//https://www.blackbox.ai/chat/MJIcVPS
 #include<stdio.h>
 #include<stdlib.h>
 
+struct node {
+    int data;
+    struct node * link;
+};
+
+
+
 void main() {
-   printf("Thank you for checking out") 
+    printf("Sorry not ready yet")
 }
