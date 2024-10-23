c0e9986f9dafcbe5b59ef4cc37010b22d4c24f8b
jeswin <jeswinpeter92@gmail.com>
2024-10-23 20:28:17 +0530

    HackerRank solution

---
+++ Different/pattern.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main() {
+    printf("Enter num less than 10: ");
+
+    int n;
+    scanf("%d", &n);
+    int min=0;
+    for(int i=1;i<=2*n-1;i++){
+        for(int j=1;j<=2*n-1;j++){
+            int a = i;
+            if(i>n) a = 2*n - i;
+            int b = j;
+            if(b>n) b = 2*n - j;
+            if(a<b) min = a;
+            else min = b;
+            printf("%d ",n+1-min);
+        }
+        printf("\n");
+    }
+}
